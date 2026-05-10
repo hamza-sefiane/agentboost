53d02dc6f38c2254bec2b0bc3fbe079cc40abff7
Sefianehamza <hamza95340@hotmail.fr>
2026-05-11 00:25:03 +0200

    force prod env before symfony runtime

--- api/index.php
+++ api/index.php
@@ -3,13 +3,15 @@
 use App\Kernel;
 
 $_SERVER['APP_ENV'] = $_SERVER['APP_ENV'] ?? 'prod';
+$_ENV['APP_ENV'] = $_ENV['APP_ENV'] ?? 'prod';
+putenv('APP_ENV=prod');
+
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? '0';
+$_ENV['APP_DEBUG'] = $_ENV['APP_DEBUG'] ?? '0';
+putenv('APP_DEBUG=0');
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel(
-        $_SERVER['APP_ENV'],
-        (bool) $_SERVER['APP_DEBUG']
-    );
+    return new Kernel('prod', false);
 };
