07133c85c4997b1a3fc9e04c07d7fca42964734e
Sefianehamza <hamza95340@hotmail.fr>
2026-05-11 18:00:06 +0200

    fix vercel entrypoint without dotenv

--- api/index.php
+++ api/index.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod';
+$debug = ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false) === '1';
+
+$kernel = new Kernel($env, $debug);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
