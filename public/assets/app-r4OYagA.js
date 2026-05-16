8c2c646511397e3e3ad754bfaa7ce50c98ffe94d
Sefianehamza <hamza95340@hotmail.fr>
2026-05-16 19:15:13 +0200

    track compiled public assets

---
+++ public/assets/app-r4OYagA.js
@@ -0,0 +1,22 @@
+import "./bootstrap.js";
+
+import "./styles/app.css";
+import "./styles/home.css";
+import "./styles/admin.css";
+import "./styles/property-create.css";
+import "./styles/property-edit.css";
+import "./styles/property-list.css";
+import "./styles/register.css";
+import "./styles/reset-password.css";
+import "./styles/goodbye.css";
+import "./styles/login.css";
+import "./styles/contact.css";
+
+import "./js/home.js";
+import "./js/admin.js";
+import "./js/property-create.js";
+import "./js/property-edit.js";
+import "./js/property-list.js";
+import "./js/register.js";
+import "./js/reset-password.js";
+import "./js/login.js";
